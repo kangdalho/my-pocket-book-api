500b7729935bcec02a3657eef76c93e80e8bc700
choyooseok <bluesbeat93@gmail.com>
2025-05-29 20:39:15 +0900

    feat : review 엔티티

--- src/main/java/com/nbcamp/mypocketbookapi/entity/Review.java
+++ src/main/java/com/nbcamp/mypocketbookapi/entity/Review.java
@@ -2,6 +2,7 @@ package com.nbcamp.mypocketbookapi.entity;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -10,8 +11,30 @@ import lombok.NoArgsConstructor;
 @Table(name = "reviews")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Review extends BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "content_id", nullable = false)
+    private Content content;
+
+    @Column
+    private int rating;
+
+    @Lob
+    private String text;
+
+    @Builder
+    public Review(Member member, Content content, int rating, String text) {
+        this.member = member;
+        this.content = content;
+        this.rating = rating;
+        this.text = text;
+    }
 }
