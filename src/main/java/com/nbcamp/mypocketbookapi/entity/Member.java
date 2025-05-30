f0c869ed53cd9caf2fedfcc5bddcf0a1782e08cd
Kang Dal Ho <d4lh0w@naver.com>
2025-05-30 09:36:12 +0900

    feat:멤버 엔티티 생성

--- src/main/java/com/nbcamp/mypocketbookapi/entity/Member.java
+++ src/main/java/com/nbcamp/mypocketbookapi/entity/Member.java
@@ -13,5 +13,16 @@ public class Member extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false,unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false,length = 50)
+    private String nickname;
 
+    public Member(String email, String password, String nickname) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+    }
 }
