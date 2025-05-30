d8d00c4f6d84a8df7b775f215bb4309a88656bba
choyooseok <bluesbeat93@gmail.com>
2025-05-30 15:05:00 +0900

    feat : review 생성 기능 구현

---
+++ src/main/java/com/nbcamp/mypocketbookapi/dto/ReviewResponseDto.java
@@ -0,0 +1,35 @@
+package com.nbcamp.mypocketbookapi.dto;
+
+import java.time.LocalDateTime;
+
+import com.nbcamp.mypocketbookapi.entity.Review;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class ReviewResponseDto {
+
+	private Long id;
+	private Long memberId;
+	private Long contentId;
+	private Integer rating;
+	private String text;
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
+
+
+	public ReviewResponseDto(Review review) {
+		this.id = review.getId();
+		this.memberId = review.getMember().getId();
+		this.contentId = review.getContent().getId();
+		this.rating = review.getRating();
+		this.text = review.getText();
+		this.createdAt = review.getCreatedAt();
+		this.updatedAt = review.getUpdatedAt();
+
+	}
+}
