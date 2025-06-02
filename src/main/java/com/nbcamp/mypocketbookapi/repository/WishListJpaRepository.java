342d575a746885b2c1fb11430c74918e5b9ccaec
강다연 <dysy9799@naver.com>
2025-06-02 12:54:23 +0900

    chore: 이름 변경

--- src/main/java/com/nbcamp/mypocketbookapi/repository/WishListJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nbcamp.mypocketbookapi.repository;
-
-import com.nbcamp.mypocketbookapi.entity.WishList;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WishListJpaRepository extends JpaRepository<WishList, Long> {
-}
