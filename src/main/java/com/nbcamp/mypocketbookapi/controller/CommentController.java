77a461630d83efb2aa5dd3b42d6209e188aa83d6
ghkdrudals75 <ghkdrudals75@naver.com>
2025-06-06 13:30:46 +0900

    "댓글 조회 기능 구현"

--- src/main/java/com/nbcamp/mypocketbookapi/controller/CommentController.java
+++ src/main/java/com/nbcamp/mypocketbookapi/controller/CommentController.java
@@ -1,15 +1,18 @@
 package com.nbcamp.mypocketbookapi.controller;
 
-import com.nbcamp.mypocketbookapi.dto.CommentRequest;
+import com.nbcamp.mypocketbookapi.dto.comment.request.CommentRequest;
+import com.nbcamp.mypocketbookapi.dto.comment.response.CommentResponse;
 import com.nbcamp.mypocketbookapi.entity.Comment;
 import com.nbcamp.mypocketbookapi.service.CommentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/comments")
 public class CommentController {
 
     private final CommentService commentService;
@@ -18,5 +21,9 @@ public class CommentController {
     public Comment create(@RequestBody CommentRequest request) {
         return commentService.createComment(request);
 
+    }
+    @GetMapping
+    public ResponseEntity<List<CommentResponse>> getAllComments() {
+        return ResponseEntity.ok(commentService.getAllComments());
     }
 }
