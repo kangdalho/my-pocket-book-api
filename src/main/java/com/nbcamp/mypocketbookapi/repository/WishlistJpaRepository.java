820dfc752c500eafa73363118a18d96503f2a7ef
강다연 <dysy9799@naver.com>
2025-06-09 09:17:34 +0900

    feat : 위시리스트에 등록된 콘텐츠 삭제하는 기능 구현 (#39)

--- src/main/java/com/nbcamp/mypocketbookapi/repository/WishlistJpaRepository.java
+++ src/main/java/com/nbcamp/mypocketbookapi/repository/WishlistJpaRepository.java
@@ -5,8 +5,11 @@ import com.nbcamp.mypocketbookapi.entity.Wishlist;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface WishlistJpaRepository extends JpaRepository<Wishlist, Long> {
 
     List<Wishlist> findByMember(Member member);
+
+    Optional<Wishlist> findById(Long id);
 }
