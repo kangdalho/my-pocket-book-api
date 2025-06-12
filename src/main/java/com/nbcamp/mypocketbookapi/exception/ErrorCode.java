051656f7f08993e3cd8a80eac0981b73d0a35472
Jong Hyeon Lee <ljh0990@naver.com>
2025-06-12 12:44:34 +0900

    fix : ErrorCode

--- src/main/java/com/nbcamp/mypocketbookapi/exception/ErrorCode.java
+++ src/main/java/com/nbcamp/mypocketbookapi/exception/ErrorCode.java
@@ -21,8 +21,7 @@ public enum ErrorCode implements BaseCode {
     UNAUTHORIZED_CONTENT_DELETION(DomainType.CONTENT, HttpStatus.FORBIDDEN, "작성자만 삭제 할수있습니다."),
     DUPLICATE_CONTENT(DomainType.CONTENT, HttpStatus.CONFLICT, "이미 등록된 도서입니다."),
     SIGNUP_REQUIRED(DomainType.MEMBER, HttpStatus.UNAUTHORIZED, "회원가입이 필요합니다."),
-    UNAUTHORIZED(DomainType.AUTH, HttpStatus.UNAUTHORIZED, "로그인이 필요합니다."),
-    REVIEW_LIKE_NOT_FOUND(DomainType.REVIEW_LIKE, HttpStatus.NOT_FOUND, "리뷰 좋아요가 존재하지 않습니다.");
+    REVIEW_LIKE_NOT_FOUND(DomainType.REVIEW_LIKE, HttpStatus.NOT_FOUND, "리뷰 좋아요가 존재하지 않습니다."),
 
     // Review
     REVIEW_NOT_FOUND(DomainType.REVIEW, HttpStatus.NOT_FOUND, "리뷰를 찾을 수 없습니다."),
