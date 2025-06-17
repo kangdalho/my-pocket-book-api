bea48d6fe53391478c900aee25b705b7ee1bc393
강다연 <dysy9799@naver.com>
2025-06-17 15:17:32 +0900

    N+1문제 해결

--- src/main/java/com/nbcamp/mypocketbookapi/repository/WishlistJpaRepository.java
+++ src/main/java/com/nbcamp/mypocketbookapi/repository/WishlistJpaRepository.java
@@ -5,11 +5,14 @@ import com.nbcamp.mypocketbookapi.entity.Wishlist;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WishlistJpaRepository extends JpaRepository<Wishlist, Long> {
 
+    @EntityGraph(attributePaths = {"member", "content"})
     Page<Wishlist> findByMember(Member member, @ParameterObject Pageable pageable);
+
     boolean existsByMemberAndIsbn(Member member, String isbn);
 
 }
