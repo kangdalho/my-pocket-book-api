7be59f8a408f2784742491ae2e4b189201dde047
Kang Dal Ho <d4lh0w@naver.com>
2025-06-19 10:55:49 +0900

    fix: Login,SignUp Request Dto 수정

--- src/main/java/com/nbcamp/mypocketbookapi/dto/member/request/LoginRequestDto.java
+++ src/main/java/com/nbcamp/mypocketbookapi/dto/member/request/LoginRequestDto.java
@@ -1,22 +1,22 @@
 package com.nbcamp.mypocketbookapi.dto.member.request;
 
-
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequestDto {
 
     @Email(message = "올바른 이메일 형식을 입력해주세요.")
     @NotBlank(message = "이메일은 필수입력사항입니다.")
-    private final String email;
+    private String email;
 
     @Size(min = 8, message = "비밀번호는 8자 이상이어야 합니다.")
     @NotBlank(message = "비밀번호는 필수입력사항입니다.")
-    private final String password;
-
+    private String password;
 }
--- src/main/java/com/nbcamp/mypocketbookapi/dto/member/request/SignupRequestDto.java
+++ src/main/java/com/nbcamp/mypocketbookapi/dto/member/request/SignupRequestDto.java
@@ -1,27 +1,26 @@
 package com.nbcamp.mypocketbookapi.dto.member.request;
 
-
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 public class SignupRequestDto {
 
     @Email(message = "올바른 이메일 형식을 입력해주세요.")
     @NotBlank(message = "이메일은 필수입력사항입니다.")
-    private final String email;
+    private String email;
 
     @Size(min = 8, message = "비밀번호는 8자 이상이어야 합니다.")
     @NotBlank(message = "비밀번호는 필수입력사항입니다.")
-    private final String password;
+    private String password;
 
     @NotNull(message = "닉네임은 필수입력사항입니다.")
-    private final String nickname;
-
-
+    private String nickname;
 }
