b42aebb517831b7410fb3a3dba3cfdd931a0d18e
강다연 <dysy9799@naver.com>
2025-06-19 14:45:44 +0900

    swagger ui 적용

--- src/main/java/com/nbcamp/mypocketbookapi/controller/WishlistController.java
+++ src/main/java/com/nbcamp/mypocketbookapi/controller/WishlistController.java
@@ -5,6 +5,8 @@ import com.nbcamp.mypocketbookapi.common.LoginMember;
 import com.nbcamp.mypocketbookapi.common.ResponseCode;
 import com.nbcamp.mypocketbookapi.dto.wishlist.WishlistResponseDto;
 import com.nbcamp.mypocketbookapi.service.WishlistService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
@@ -17,10 +19,12 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@Tag(name = "위시리스트 API", description = "위시리스트 관련 API")
 public class WishlistController {
 
     private final WishlistService wishlistService;
 
+    @Operation(summary = "위시리스트에 찜 등록", description = "로그인한 사용자가 특정 콘텐츠를 위시리스트에 찜 등록합니다.")
     @PostMapping("/contents/{contentId}/wishlists")
     public ResponseEntity<BaseResponse<WishlistResponseDto>> saveWishlist(@LoginMember Long memberId, @PathVariable Long contentId) {
 
@@ -29,6 +33,7 @@ public class WishlistController {
         return ResponseEntity.status(HttpStatus.CREATED).body(BaseResponse.success(ResponseCode.SUCCESS_CREATED, wishlistResponseDto));
     }
 
+    @Operation(summary = "위시리스트 전체 조회", description = "위시리스트 전체 목록을 조회합니다.")
     @GetMapping("/wishlists")
     public ResponseEntity<BaseResponse<Page<WishlistResponseDto>>> listAll(@LoginMember Long memberId, @ParameterObject @PageableDefault(size = 10, page = 0) Pageable pageable) {
 
@@ -37,6 +42,7 @@ public class WishlistController {
         return ResponseEntity.ok(BaseResponse.success(ResponseCode.SUCCESS_OK, wishlists));
     }
 
+    @Operation(summary = "위시리스트 삭제", description = "위시리스트에 등록된 콘텐츠를 삭제합니다.")
     @DeleteMapping("/wishlists/{wishlistId}")
     public ResponseEntity<BaseResponse<Void>> deleteByWish(@PathVariable Long wishlistId) {
 
