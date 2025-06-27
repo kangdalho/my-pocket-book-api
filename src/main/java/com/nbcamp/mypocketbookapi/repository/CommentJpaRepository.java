5d5989121ae15e165041511a4843233de0791079
ghkdrudals75 <ghkdrudals75@naver.com>
2025-06-27 11:55:50 +0900

    "댓글 조회 페이징 n+1 해결"

--- src/main/java/com/nbcamp/mypocketbookapi/repository/CommentJpaRepository.java
+++ src/main/java/com/nbcamp/mypocketbookapi/repository/CommentJpaRepository.java
@@ -4,9 +4,11 @@ import com.nbcamp.mypocketbookapi.entity.Comment;
 import com.nbcamp.mypocketbookapi.entity.Review;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentJpaRepository extends JpaRepository<Comment, Long> {
 
+    @EntityGraph(attributePaths = {"member"})
     Page<Comment> findByReview(Review review, Pageable pageable);
 }
